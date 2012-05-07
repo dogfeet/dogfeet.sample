819098be6d9f239a572bbb4012e15e209ded843f
Changwoo Park <pismute@gmail.com>
2012-05-07 21:22:45 +0900

    Add iamport.py

---
+++ iamport/iamport.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+# Echo client program
+import socket
+import sys
+import time
+import re
+
+def tryToConnect( argv ):
+    ip=argv[0]
+    port=int(argv[1])
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM )
+
+    try:
+        sock.connect( (ip, port) )
+    except:
+        print "{0} {1}".format( ip, port )
+
+    sock.close()
+
+if __name__ == '__main__':
+
+    stime=time.time()
+
+    file = open(sys.argv[1])
+
+    jobs = []
+
+    while True:
+        line = file.readline()
+
+        if not line:
+            break
+
+        opts = re.split('\s*', line)
+
+        if len(opts) > 2:
+            job = tryToConnect( opts )
+            jobs.append( job )
+        else:
+            print opts[0]
+
+    file.close()
+
+    etime=time.time()
+
+    print 'elapsed %f' % (etime - stime)
+
