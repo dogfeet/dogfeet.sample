5849188cf83e4cb9582f02a705f8b11f29411a07
Changwoo Park <pismute@gmail.com>
2012-06-07 17:06:55 +0900

    Complete iamport.js

--- iamport/iamport.js
+++ iamport/iamport.js
@@ -2,13 +2,31 @@
 
 //Echo client program
 
-function tryToConnect( ip, port ){
-    port=parseInt(port);
-
-    console.log([ip, port]);
-}
-
+var net = require('net');
 var fs = require('fs');
+
+var stime=new Date().getTime();
+var done=0;
+
+function tryToConnect( ip, port ){
+    done++;
+
+    var socket = net.createConnection(port, ip);
+
+    socket.on('error', function(err){
+        console.log( ip + ' ' + port );
+    }).on('connect', function(connect) {
+        socket.destroy();
+    }).on('close', function(had_error){
+        done--;
+
+        if(done == 0 ){
+            var etime=new Date().getTime();
+
+            console.log( "elapsed(ms) " + (etime - stime) );
+        }
+    });
+}
 
 fs.readFile(process.argv[2], 'utf-8', function(err, data){
     var lines = data.split('\n');
@@ -23,4 +41,5 @@ fs.readFile(process.argv[2], 'utf-8', function(err, data){
         }
     });
 });
+
 
